6b2f54de932234be92fccbdab87adcd0df0d281d
gunjanbajaj1 <gunjanbajaj106@gmail.com>
2023-08-08 20:45:49 +0530

    Day-2 code algo added

--- 2.sequenceOfNumber.js
+++ 2.sequenceOfNumber.js
@@ -1,3 +1,16 @@
+//Algorithm
+
+/*
+1. 1st Loop for checking the 1st variable 
+2. Compare it with coming element if it is same then leave it other wise swap the variable and break the second loop
+3. Continue this process till the end of array
+
+
+
+
+
+*/
+
 function sequence1(arr){
     for(let i=0;i<arr.length;i++){
         for(let j= i+1;j<arr.length;j++){
